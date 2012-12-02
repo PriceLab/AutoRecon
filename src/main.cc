3332c53e3b5501f963254d2a931d15e4bbfc941b
Matthew Benedict <mattb112885@autorecon.(none)>
2012-12-02 13:23:05 -0600

    Shell of programs

--- src/main.cc
+++ src/main.cc
@@ -12,11 +12,14 @@ int main(int argc, char *argv[]) {
     cout << "Usage: ./a.out [biochem_json] [model_json] " << endl;
     exit(EXIT_FAILURE);
   }
-  // Parse JSON files.
+  //! Parse JSON files.
   Problem problem;
   loadBiochemistryFile(argv[1], problem.biochem);
   loadModelFile(argv[2], problem.model);
   validateProblem(problem);
+
+  //! Identify synonymous reactions
+  map<ReactionPtr, vector<ReactionPtr> > = computeSynonyms(problem);
 
   return 0;
 }
--- src/pathUtils.cc
+++ src/pathUtils.cc
@@ -1,18 +1,95 @@
 #include "pathUtils.h"
 #include "Models.h"
 
+#include <map>
+#include <vector>
+#include <set>
+
 using std::vector;
 using std::map;
 
 bool isExternalCompound(ModelCompoundPtr compound, Problem& problem)
 {
-	// Model compound points to model compartment ...
-	ModelCompartmentMapIterator miter = problem.model.modelCompartmentList.find(compound->modelCompartmentUuid);
+  //! Based on the name of the compartment in which a compound is found,
+  //! identify whether or not it is extra-cellular.
+  //!
+  //! Returns TRUE if the compound is extra-cellular and FALSE otherwise.
 
-	// ... which points to compartment ...
-	CompartmentMapIterator citer = problem.biochem.compartmentList.find(miter->second->compartmentUuid);
+  // Model compound points to model compartment ...
+  ModelCompartmentMapIterator miter = problem.model.modelCompartmentList.find(compound->modelCompartmentUuid);
+  
+  // ... which points to compartment ...
+  CompartmentMapIterator citer = problem.biochem.compartmentList.find(miter->second->compartmentUuid);
+  
+  // ... which has a name.
+  if (citer->second->name == "Extracellular") return true;
+  return false;
+}
 
-	// ... which has a name.
-	if (citer->second->name == "Extracellular") return true;
-	return false;
+
+////INCOMPLETE functions
+set<CompoundPtr> getCofactorList(Problem &problem) {
+  //! Given a problem structure, iterate over the biochemistry
+  //! object to obtain a list of metabolites (compounds) that are
+  //! identified as cofactors.
+  //!
+  //! Returns a list of pointers to compounds that are labeled as "cofactors"
+  //! in the biochemistry object. This uses the metabolite attribute, not the
+  //! reaction-specific attribute.
+
+  set<CompoundPtr> cofactorList;
+
+  return cofactorList;
+}
+
+map<ReactionPtr, vector<ReactionPtr> > computeSynonyms(Problem &problem) {
+  //! Given a problem structure, iterate over the biochemistry object
+  //! to identify sets of reactions with identical metabolites except
+  //! for those that are identified as cofactors either in the metabolite
+  //! list or in the reaction itself.
+  //!
+  //! It is assumed that a metabolite that is declared as a cofactor
+  //! is a cofactor regardless of what compartment it is found in.
+  //!
+  //! Returns a mapping from each reaction's pointer to a list of that reaction's
+  //! synonyms (including itself)
+
+  map<ReactionPtr, vector<ReactionPtr> > synList;
+
+  // List of metabolites labeled as cofactors in the biochemistry object
+  set<CompoundPtr> cofactorList;
+
+  // Mapping from each reaction to a list of non-cofactor metabolites found within it
+  map<ReactionPtr, vector<CompoundPtr> > netReactions;
+
+  //! Get list of cofactor metabolites
+  cofactorList = getCofactorList(problem);
+
+  //! Iterate over reactions and identify their net reaction.
+  //! NOTE - This would be more efficient though if we make a map<vector<CompoundPtr>, vector<ReactionPtr> >
+  //! - this is only possible if the map data structure can correctly tell whether two vector<CompountPtr>
+  //! are the same or not.
+  // 
+  // for each ReactionPtr
+  //    make a vector of CompoundPtr
+  //    for each metabolite in that reaction
+  //       if that metabolite is labled as a cofactor within that reaction:
+  //          continue
+  //       if that metabolite is labeled as a cofactor within the cofactor list:
+  //          continue
+  //       Add the metabolite to the vector of CompoundPtr
+  //    Add cofactorList to netReactions with ReactionPtr as a pointer.
+
+
+  //! Itentify lists of synonymous reactions.
+  //! This part would be trivial if we can use the alternate implementation described above.
+  //! As it is in this sketch it would be O(N^2)
+  // For each ReactionPtr:
+  //   Make a vector<ReactionPtr>
+  //   For each ReactionPtr:
+  //    If the vector<CompoundPtr> is the same as the reference:
+  //      add the query reaction to the vector<ReactionPtr>
+  //   Add ReactionPtr-->vector<ReactionPtr> to synList
+
+  return synList;
 }
